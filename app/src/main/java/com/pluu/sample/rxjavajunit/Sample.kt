2e266ddce030ad016b945957ab1124cfc20e66a8
pluu <pluulove@gmail.com>
2022-02-18 00:57:53 +0900

    테스트 케이스 체크

--- app/src/main/java/com/pluu/sample/rxjavajunit/Sample.kt
+++ app/src/main/java/com/pluu/sample/rxjavajunit/Sample.kt
@@ -3,19 +3,14 @@ package com.pluu.sample.rxjavajunit
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.kotlin.plusAssign
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
 
 class Sample : AutoCloseable {
-    private val disposable = CompositeDisposable()
+    private val compositeDisposable = CompositeDisposable()
 
-    fun test(callback: (Int) -> Unit) {
-        disposable += Observable.just(1)
-            .delay(2, TimeUnit.SECONDS)
-            .map {
-                it * it
-            }
+    fun testParam1(callback: (data: Int) -> Unit) {
+        Observable.just(1)
             .subscribeOn(Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnSubscribe {
@@ -26,12 +21,67 @@ class Sample : AutoCloseable {
             }
             .subscribe {
                 callback(it)
+            }.addComposable(compositeDisposable)
+    }
+
+    fun testParam2(callback: (data: List<Int>, key: Int?) -> Unit) {
+        Observable.just(1)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                printNow("Block Start")
             }
+            .doOnTerminate {
+                printNow("Block End")
+            }
+            .subscribe {
+                callback((1..10).toList(), it)
+            }.addComposable(compositeDisposable)
+    }
+
+    fun testListenerParam1(callback: SampleListener) {
+        Observable.just(1)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                printNow("Block Start")
+            }
+            .doOnTerminate {
+                printNow("Block End")
+            }
+            .subscribe {
+                callback.callback1(1)
+            }.addComposable(compositeDisposable)
+    }
+
+    fun testListenerParam2(callback: SampleListener) {
+        Observable.just(1)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                printNow("Block Start")
+            }
+            .doOnTerminate {
+                printNow("Block End")
+            }
+            .subscribe {
+                callback.callback2((1..10).toList(), it)
+            }.addComposable(compositeDisposable)
     }
 
     override fun close() {
-        if (disposable.isDisposed) {
-            disposable.clear()
+        if (compositeDisposable.isDisposed) {
+            compositeDisposable.clear()
         }
     }
+
+    interface SampleListener {
+        fun callback1(data: Int)
+
+        fun callback2(data: List<Int>, key: Int?)
+    }
+}
+
+fun Disposable.addComposable(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
 }
--- app/src/test/java/com/pluu/sample/rxjavajunit/SampleTest.kt
+++ app/src/test/java/com/pluu/sample/rxjavajunit/SampleTest.kt
@@ -2,19 +2,23 @@ package com.pluu.sample.rxjavajunit
 
 import com.pluu.sample.rxjavajunit.utils.RxJavaUncaughtErrorRule
 import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.junit.After
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.*
 
 class SampleTest {
 
     @get:Rule
     val uncaughtRxJavaErrors = RxJavaUncaughtErrorRule()
+
+    private lateinit var sample: Sample
 
     @Before
     fun setUp() {
@@ -26,22 +30,178 @@ class SampleTest {
         RxAndroidPlugins.setMainThreadSchedulerHandler {
             Schedulers.trampoline()
         }
+
+        sample = Sample()
     }
 
     @After
     fun tearDown() {
         RxAndroidPlugins.reset()
         RxJavaPlugins.reset()
+
+        sample.close()
     }
 
-    @Test
-    fun test1() {
-        Sample().use { sample ->
-            sample.test { value ->
-                assertTrue(value > 100)
+    ///////////////////////////////////////////////////////////////////////////
+    // Sample
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Test(expected = AssertionError::class)
+    fun sample1_fail() {
+        Single.just(1)
+            .subscribe { value ->
+                fail("Result:$value")
             }
+    }
+
+    private fun testCase(callback: (Int) -> Unit) {
+        Single.just(1)
+            .subscribe { value ->
+                callback(value)
+            }
+    }
+
+    @Test(expected = AssertionError::class)
+    fun sample_mockito1() {
+        val mockCallback: (Int) -> Unit = mock()
+        testCase(mockCallback)
+
+        val paramCapture = argumentCaptor<Int>()
+        verify(mockCallback).invoke(paramCapture.capture())
+
+        fail("Result:${paramCapture.firstValue}")
+    }
+
+    @Test(expected = AssertionError::class)
+    fun sample_mockito2() {
+        val mockCallback: (Int) -> Unit = mock()
+        testCase(mockCallback)
+
+        verify(mockCallback).invoke(eq(0))
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Parameter 1 Test
+    ///////////////////////////////////////////////////////////////////////////
+
+    // value : 1
+    // 해당 케이스는 실패해야지 정상
+    @Test(expected = AssertionError::class)
+    fun t1_fail() {
+        sample.testParam1 { value ->
+            assertTrue(value > 100)
+        }
+    }
+
+    // value : 1
+    // 해당 케이스는 실패해야지 정상
+    @Test(expected = AssertionError::class)
+    fun t1_success() {
+        sample.testParam1 { value ->
+            assertTrue(value > 100)
         }
 
         uncaughtRxJavaErrors.assertNoErrors()
+    }
+
+    // value : 1
+    // 해당 케이스는 실패해야지 정상
+    // testParam1로 넘겨진 콜백이 불려지기전에 실패한 경우는 불가능
+    @Test(expected = AssertionError::class)
+    fun t1_success2() {
+        val mockCallback: (Int) -> Unit = mock()
+        sample.testParam1(mockCallback)
+
+        val paramCapture = argumentCaptor<Int>()
+        verify(mockCallback).invoke(paramCapture.capture())
+        assertTrue(paramCapture.firstValue > 100)
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Parameter 2 Test
+    ///////////////////////////////////////////////////////////////////////////
+
+    // list : 1..10
+    // key : 1
+    // 해당 케이스는 실패해야지 정상
+    @Test(expected = AssertionError::class)
+    fun t2_fail() {
+        sample.testParam2 { list, _ ->
+            assertTrue(list.isEmpty())
+        }
+    }
+
+    // list : 1..10
+    // key : 1
+    // 해당 케이스는 실패해야지 정상
+    @Test(expected = AssertionError::class)
+    fun t2_success() {
+        sample.testParam2 { list, _ ->
+            assertTrue(list.isEmpty())
+        }
+
+        uncaughtRxJavaErrors.assertNoErrors()
+    }
+
+    // list : 1..10
+    // key : 1
+    // 해당 케이스는 실패해야지 정상
+    // testParam2로 넘겨진 콜백이 불려지기전에 실패한 경우는 불가능
+    @Test(expected = AssertionError::class)
+    fun t2_success2() {
+        val mockCallback: (List<Int>, Int?) -> Unit = mock()
+        sample.testParam2(mockCallback)
+
+        val argumentCaptor1 = argumentCaptor<List<Int>>()
+        val argumentCaptor2 = argumentCaptor<Int>()
+        verify(mockCallback, times(1)).invoke(argumentCaptor1.capture(), argumentCaptor2.capture())
+
+        assertEquals((1..10).toList(), argumentCaptor1.firstValue)
+        assertNull(argumentCaptor2.firstValue)
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Listener Param 1 Test
+    ///////////////////////////////////////////////////////////////////////////
+
+    // 해당 케이스는 실패해야지 정상
+    @Test(expected = AssertionError::class)
+    fun t3_fail() {
+        sample.testListenerParam1(object : Sample.SampleListener {
+            override fun callback1(data: Int) {
+                fail("실패 테스트1")
+            }
+
+            override fun callback2(data: List<Int>, key: Int?) {
+                fail("실패 테스트2")
+            }
+        })
+    }
+
+    // 해당 케이스는 실패해야지 정상
+    @Test(expected = AssertionError::class)
+    fun t3_success() {
+        sample.testListenerParam1(object : Sample.SampleListener {
+            override fun callback1(data: Int) {
+                fail("실패 테스트1")
+            }
+
+            override fun callback2(data: List<Int>, key: Int?) {
+                fail("실패 테스트2")
+            }
+        })
+
+        uncaughtRxJavaErrors.assertNoErrors()
+    }
+
+    // 해당 케이스는 실패해야지 정상
+    // testListenerParam1로 넘겨진 콜백이 불려지기전에 실패한 경우는 불가능
+    @Test(expected = AssertionError::class)
+    fun t3_success2() {
+        val mockCallback: Sample.SampleListener = mock()
+        sample.testListenerParam1(mockCallback)
+
+        verify(mockCallback, never()).callback1(any())
+        verify(mockCallback, never()).callback2(any(), anyOrNull())
     }
 }
